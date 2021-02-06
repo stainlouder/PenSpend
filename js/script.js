b803ae8d0cd1d68e08ee84c084b963525d4396df
stainlouder <stainlouder@gmail.com>
2021-02-06 21:45:51 +0300

    Header section. Adaptive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-alert('Hello'); 
+alert('Hello');
+
+$(document).ready(function() {
+    $('.icon-menu').click(function(event) {
+        $('.menu-icon, .menu__body').toggleClass('_active');
+        $('body').toggleClass('lock');
+    });
+});
